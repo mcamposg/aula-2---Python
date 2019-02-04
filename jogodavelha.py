a86f95b79c38997cb2dee747a6136f644e97ba80
todomundo <labasence@gmail.com>
2019-02-04 16:01:07 -0200

    com ajuda do professor jogo ok

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,5 +1,5 @@
-#O tabuleiro
-velha ="""               Posições
+# O tabuleiro
+velha = """               Posições
     |   |      7 | 8 | 9
  ---+---+---  ---+---+---
     |   |      4 | 5 | 6
@@ -8,36 +8,36 @@ velha ="""               Posições
 """
 
 posições = [
-       None,  # Elemento adicionado para facilitar índices
-      (5, 1), # 1
-      (5, 5), # 2
-      (5, 9), # 3
-      (3, 1), # 4
-      (3, 5), # 5
-      (3, 9), # 6
-      (1, 1), # 7
-      (1, 5), # 8
-      (1, 9), # 9
-    ]
+    None,  # Elemento adicionado para facilitar índices
+    (5, 1),  # 1
+    (5, 5),  # 2
+    (5, 9),  # 3
+    (3, 1),  # 4
+    (3, 5),  # 5
+    (3, 9),  # 6
+    (1, 1),  # 7
+    (1, 5),  # 8
+    (1, 9),  # 9
+]
 
 ganho = [
-          [ 1, 2, 3], # Linhas
-          [ 4, 5, 6],
-          [ 7, 8, 9],
-          [ 7, 4, 1], # Colunas
-          [ 8, 5, 2],
-          [ 9, 6, 3],
-          [ 7, 5, 3], # Diagonais
-          [ 1, 5, 9]
-        ]
+    [1, 2, 3],  # Linhas
+    [4, 5, 6],
+    [7, 8, 9],
+    [7, 4, 1],  # Colunas
+    [8, 5, 2],
+    [9, 6, 3],
+    [7, 5, 3],  # Diagonais
+    [1, 5, 9]
+]
 
 tabuleiro = []
 for linha in velha.splitlines():
     tabuleiro.append(list(linha))
 
-jogador = "X" # Começa jogando com X
+jogador = "X"  # Começa jogando com X
 jogando = True
-jogadas = 0 # Contador de jogadas - usado para saber se velhou
+jogadas = 0  # Contador de jogadas - usado para saber se velhou
 while True:
     for t in tabuleiro:  # Imprime o tabuleiro
         print("".join(t))
@@ -49,25 +49,24 @@ while True:
         break
 
     jogada = int(input("Digite a posição a jogar 1-9 (jogador %s):" % jogador))
-    if jogada<1 or jogada>9:
-            print("Posição inválida")
-            continue
+    if jogada < 1 or jogada > 9:
+        print("Posição inválida")
+        continue
 
     if tabuleiro[posições[jogada][0]][posições[jogada][1]] != " ":  # Verifica se a posição está livre
-            print("Posição ocupada.");
-            continue
+        print("Posição ocupada.")
+        continue
 
-tabuleiro[posições[jogada][0]][posições[jogada][1]] = jogador  # Marca a jogada para o jogador
+    tabuleiro[posições[jogada][0]][posições[jogada][1]] = jogador  # Marca a jogada para o jogador
 
-for p in ganho:  # Verfica se ganhou
+    for p in ganho:  # Verfica se ganhou
         for x in p:
             if tabuleiro[posições[x][0]][posições[x][1]] != jogador:
-               break
+                break
         else:  # Se o for terminar sem break, todas as posicoes de p pertencem ao mesmo jogador
             print("O jogador %s ganhou (%s): " % (jogador, p))
             jogando = False
             break
 
-jogador = "X" if jogador == "O" else "O" # Alterna jogador
-jogadas +=1 # Contador de jogadas
-
+    jogador = "X" if jogador == "O" else "O"  # Alterna jogador
+    jogadas += 1  # Contador de jogadas
